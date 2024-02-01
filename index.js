0859587d25557ed953eb363042ebd029ea158406
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-02-02 00:45:06 +0500

    appended контакты меню акции

--- index.js
+++ index.js
@@ -8,62 +8,68 @@ const { clientText } = require('./clientText.js');
 bot.use(session())
 
 function showMainMenu(ctx) {
-  ctx.reply(
-    'Меню:',
+  ctx.reply('Главное меню:',
     Markup.keyboard([
-      [{ text: "Кешбэк", request_contact: true, }, , 'Меню'],
+      [{ text: "Кешбэк", request_contact: true, }, 'Меню'],
       ['Акции', 'Контакты'],
       ['Оставить отзыв']
     ]).resize().extra()
   );
 }
 
+function showContactOptions(ctx) {
+  ctx.reply('Выберите контакт', {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Instagram', url: 'https://www.instagram.com/myata_platinum_tashkent' }, { text: 'Telegram', url: 'https://t.me/myataplatinum_uz' }],
+        [{ text: 'Youtube', url: 'https://www.youtube.com/@myata_uz' }, { text: 'Web Site', url: 'https://taplink.cc/platinummyata' }]
+      ],
+    },
+  });
+}
 
-bot.start(
-  async (ctx) => {
-    showMainMenu(ctx);
+function showTestimonialsMenu(ctx) {
+  ctx.reply('Оставьте отзыв',
+    Markup.keyboard([
+      ["🤩 Все чудесно, спасибо, 5⭐️⭐️⭐️⭐️⭐️"],
+      ["😏 Все хорошо, но на 4⭐️⭐️⭐️⭐️"],
+      ["😐 Удовлетворительно, на 3⭐️⭐️⭐️"],
+      ["😒 Не понравилось, на 2⭐️⭐️"],
+      ["😡 Оставить жалобу, 1⭐️"],
+      ['↩️ Назад']
+    ]).resize().extra()
+  );
+}
 
-    bot.on("contact", async (ctx) => {
+bot.start(async (ctx) => { showMainMenu(ctx); })
 
-      console.log('phone is:', ctx.message.contact.phone_number);
-
-      const url = "https://api-ru.iiko.services/api/1/loyalty/iiko/customer/info";
-      const urlapi = "https://api-ru.iiko.services/api/1/access_token";
-      const headersapi = { Timeout: "60" };
-
-      const bodyapi = { apiLogin: "74913501-9de" };
-      let headers = {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBcGlMb2dpbklkIjoiYjUwOThmYzMtMjFjMC00YTUwLWFhNjctMzY0NzI0MDAwNzgxIiwibmJmIjoxNzA2MzUyNjM5LCJleHAiOjE3MDYzNTYyMzksImlhdCI6MTcwNjM1MjYzOSwiaXNzIjoiaWlrbyIsImF1ZCI6ImNsaWVudHMifQ.9MpPtPtHlllnZcpRfwTp-e4G6FP28fTk4znQoyzr7w0",
-        "Timeout": "60",
-        "Content-Type": "application/json",
-      };
-      const body = {
-        phone: `${ctx.message.contact.phone_number}`,
-        type: "phone",
-        organizationId: "bbb98635-9a82-47d6-ac11-70e949865385",
-      };
-
-      let apikey = await axios.post(urlapi, bodyapi, { headersapi })
-
-      headers.Authorization = `Bearer ${apikey?.data?.token}`;
-      try {
-        let result = await axios.post(url, body, { headers });
-        ctx.reply(`
-      Номер карты: ${ctx?.message?.contact?.phone_number}\nБаланс на счету: ${result?.data?.walletBalances[0]?.balance ? result?.data?.walletBalances[0]?.balance : '0'}`)
-      }
-      catch { ctx.reply(`Номер счета не найден в системе`) }
-
-
-
-
-    });
-  })
-
-
-bot.hears('11', async (ctx) => { ctx.reply('Hey there'); })
-bot.hears('Акции', async (ctx) => { ctx.reply(`${clientText.actions}`) })
-bot.hears('Кешбэк', (ctx) => {
-  ctx.reply('Предоставить номер телефона:', Markup.keyboard([Markup.contactRequestButton('Отправить номер')]).resize().extra());
+bot.on("contact", async (ctx) => {
+  const url = "https://api-ru.iiko.services/api/1/loyalty/iiko/customer/info";
+  const urlapi = "https://api-ru.iiko.services/api/1/access_token";
+  const headersapi = { Timeout: "60" };
+  const bodyapi = { apiLogin: "74913501-9de" };
+  let headers = { "Authorization": "", "Timeout": "60", "Content-Type": "application/json", };
+  const body = { phone: `${ctx.message.contact.phone_number}`, type: "phone", organizationId: "bbb98635-9a82-47d6-ac11-70e949865385", };
+  let apikey = await axios.post(urlapi, bodyapi, { headersapi })
+  headers.Authorization = `Bearer ${apikey?.data?.token}`;
+  try {
+    let result = await axios.post(url, body, { headers });
+    ctx.reply(`Номер карты: ${ctx?.message?.contact?.phone_number}\nБаланс на счету: ${result?.data?.walletBalances[0]?.balance ? result?.data?.walletBalances[0]?.balance : '0'}`)
+  } catch { ctx.reply(`Номер счета не найден в системе`) }
 });
+
+bot.hears('Главное меню', (ctx) => { showMainMenu(ctx); });
+
+bot.hears('↩️ Назад', (ctx) => { showMainMenu(ctx); });
+
+bot.hears('Кешбэк', (ctx) => { ctx.reply('Предоставить номер телефона:', Markup.keyboard([Markup.contactRequestButton('Отправить номер')]).resize().extra()); });
+
+bot.hears('Меню', (ctx) => { ctx.reply('https://taplink.cc/platinummyata'); });
+
+bot.hears('Акции', async (ctx) => { ctx.reply(`${clientText.actions}`) })
+
+bot.hears(['Контакты'], (ctx) => { showContactOptions(ctx); });
+
+bot.hears(['Оставить отзыв'], (ctx) => { showTestimonialsMenu(ctx); });
 
 bot.launch()
